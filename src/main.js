38f87018e7489fb8936e01484f251b2ca9ff1337
Melrose <qckong23@163.com>
2020-11-22 22:10:57 +0800

    配置更新

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-import store from './store/'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
-import moment from 'moment'
-import VueDirectiveImagePreviewer from 'vue-directive-image-previewer'
 import 'vue-directive-image-previewer/dist/assets/style.css'
-
+import VueDirectiveImagePreviewer from 'vue-directive-image-previewer'
+import moment from 'moment'
+import router from './router'
+import store from './store/'
 Vue.use(VueDirectiveImagePreviewer);
 Vue.use(require('vue-moment'));
 Vue.prototype.moment = moment;
